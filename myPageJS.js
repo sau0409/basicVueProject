090e46225b300e480edd275d29a97bbe458a88e2
sau0409 <64318068+sau0409@users.noreply.github.com>
2020-08-01 05:34:43 +0530

    hiding elemnt on condition

--- myPageJS.js
+++ myPageJS.js
@@ -5,19 +5,56 @@
 
 // creating vue instance
 
+
+
 var app = new Vue({
-	el: '#app',  // mounts vue to root div id 
+	el: '#app', // mounts vue to root div id 
 	data() {
 		return {
-		heading: "Email Subscribe",
-		email: "example@hulk.com",
-		submitted: false
+			heading: "Astronomy Picture By NASA",
+			email: "example@hulk.com",
+			submitted: false,
+			imgTitle: "",
+			imgDate: "",
+			imgUrl: ""
 		}
 	},
 	methods: {
-		submitEmail(){
+		submitEmail() {
 			alert(`Submitted your mail ${this.email}`);
 			this.submitted = true; // changing submiited value once email is submited successfully to shoe message.
+		},
+		async getImageFromNasa(date) {
+			const url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY";
+			try{
+			   
+				const response = await axios.get(`${url}&date=${date}`);
+				    this.imgUrl = response.data.hdurl;
+					this.imgTitle = response.data.title;
+			}
+			catch(err){
+				console.log(err);
+			}
 		}
 	}
 })
+
+/*
+async function getImageFromNasa(date) {
+
+	try {
+
+		const response2 = await axios.get(`${url}`);
+		console.log(response2);
+
+		const response = await axios.get(`${url}&date=${date}`);
+		console.log(response);
+		this.imgUrl = response;
+
+	}
+	catch(err){
+        console.log(err);
+	}
+
+}
+*/
